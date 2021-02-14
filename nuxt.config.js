05e548a7b3b8db46fcf3a32d42a19d4426d852ea
ErodzB <honduras.eric@gmail.com>
2021-02-13 20:47:58 -0600

    Adding packages for using scss variables in vue files

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~/assets/scss/main.scss'],
+  css: ['~assets/scss/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -42,8 +42,11 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
+    '@nuxtjs/style-resources',
   ],
-
+  styleResources: {
+    scss: ['./assets/scss/*.scss'],
+  },
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
