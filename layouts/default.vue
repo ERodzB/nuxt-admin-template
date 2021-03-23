909b96c67c8c977c06e3c82865b539ebdf65989b
ErodzB <honduras.eric@gmail.com>
2021-03-23 08:25:59 -0600

    Basic Main Layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="main-app jsb">
+    <div class="main-app__side-menu"></div>
+    <Nuxt class="main-app__content" />
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.main-app {
+  @extend %flex-wrapper;
+  @extend %h-100-vh;
+  &__side-menu {
+    @extend %flex-18col-4;
+    background-color: $blue-100;
+  }
+  &__content {
+    @extend %flex-18col-13;
+    background-color: map-get($map: $main-theme, $key: 'background-color');
+  }
+}
+</style>
